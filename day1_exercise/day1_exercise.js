0a8a24b946033b0bbba19cb178c0a7903586cc89
David Durlan <david.a.durlan@gmail.com>
2019-11-18 18:03:02 +0100

    Adding Day 1 Exercise

---
+++ day1_exercise/day1_exercise.js
@@ -0,0 +1,19 @@
+// Exercise: Format the function
+
+function fizzbuzz() {
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            if (i % 5 == 0) {
+                console.log("fizzbuzz");
+            } else {
+                console.log("fizz");
+            }
+        } else {
+            if (i % 5 == 0) {
+                console.log("buzz");
+            } else {
+                console.log(i);
+            }
+        }
+    }
+}
