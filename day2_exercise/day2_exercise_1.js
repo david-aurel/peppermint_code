a94d2beed5ee31ccfbeaa0db0ac8628f4e42f8aa
David Durlan <david.a.durlan@gmail.com>
2019-11-19 12:30:38 +0100

    set up exercise files

---
+++ day2_exercise/day2_exercise_1.js
@@ -0,0 +1,13 @@
+//Write a function named logType that expects a single argument and logs a different string depending on the type/value of the argument that is passed to it. The string it logs should be one of the following:
+// "undefined!"
+// "null!"
+// "number!"
+// "not a number!"
+// "string!"
+// "boolean!"
+// "function!"
+// "object!"
+// "array!"
+// "I have no idea!"
+
+function logtype(val) {}
