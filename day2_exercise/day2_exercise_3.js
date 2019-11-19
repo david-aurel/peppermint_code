e7408995da772d1ee9deacad837b1937e1b54a33
David Durlan <david.a.durlan@gmail.com>
2019-11-19 16:33:40 +0100

    completed exercise

--- day2_exercise/day2_exercise_3.js
+++ day2_exercise/day2_exercise_3.js
@@ -1,1 +1,10 @@
 //Write a while or for loop that counts down from 10 to 1, logging each number to the console.
+
+var i = 10;
+
+while (i >= 0) {
+    console.log(i + "...");
+    i--;
+}
+
+console.log("IT'S THE FINAL COUNTDOWN! (DEEDOODEEDOOO DEEDOODEETDEETDOO)");
