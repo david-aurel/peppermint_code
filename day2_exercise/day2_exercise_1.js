f852f59314d701960c4edd43e679d16a376651f2
David Durlan <david.a.durlan@gmail.com>
2019-11-19 15:37:34 +0100

    completed exercise

--- day2_exercise/day2_exercise_1.js
+++ day2_exercise/day2_exercise_1.js
@@ -1,4 +1,5 @@
 //Write a function named logType that expects a single argument and logs a different string depending on the type/value of the argument that is passed to it. The string it logs should be one of the following:
+//
 // "undefined!"
 // "null!"
 // "number!"
@@ -10,4 +11,40 @@
 // "array!"
 // "I have no idea!"
 
-function logtype(val) {}
+function logType (val) {
+    if (typeof val == "undefined") {
+        console.log("undefined!");
+    } else if (val == null) {
+        console.log("null!");
+    } else if (Array.isArray(val)) {
+        console.log("array!");
+    } else if (typeof val == 'object') {
+        console.log("object!");
+    } else if (typeof val == "function") {
+        console.log("function!");
+    } else if (typeof val == "boolean") {
+        console.log("boolean!");
+    } else if (typeof val == "string") {
+        console.log("string!");
+    } else if (isNaN(val)) {
+        console.log("not a number!");
+    } else if (typeof val == "number") {
+        console.log("number!");
+    } else if (val == null) {
+        console.log("null!");
+    } else {
+        console.log("I have no idea!");
+    }
+}
+
+
+//calling the function with different arguments to test if it's working
+logType(undefined);
+logType(null);
+logType(1);
+logType(NaN);
+logType('string');
+logType(true);
+logType(function fn() {});
+logType({a: 'a'});
+logType([1, 2, 3]);
