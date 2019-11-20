b5f88ae1bd0ee38634884904a1eb543974b6024c
David Durlan <david.a.durlan@gmail.com>
2019-11-20 11:49:19 +0100

    first commit

---
+++ day3_exercise/day3_exercise_1.js
@@ -0,0 +1,8 @@
+// Write a function that takes any number of numbers as arguments and returns the sum of those numbers.
+
+// For Testing
+sum(5, 10); //15
+
+sum(5, 10, 15); //30;
+
+sum(5, 10, 15, 100, 200); //330
---
+++ day3_exercise/day3_exercise_2.js
@@ -0,0 +1,8 @@
+// Write a function that takes another function as an argument. It should wait 1.5 seconds and then run the function that was passed in.
+
+waitThenRun(function() {
+    console.log('Hello!');
+    waitThenRun(function() {
+        console.log('Goodbye!');
+    }); // logs 'Goodbye!' 1.5 seconds later
+}); // logs 'Hello!' 1.5 seconds later
