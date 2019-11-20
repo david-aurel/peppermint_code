5299e0b20f10fa2a56c4f0278a94fd674d89d962
David Durlan <david.a.durlan@gmail.com>
2019-11-20 12:29:42 +0100

    completed exercise

--- day3_exercise/day3_exercise_2.js
+++ day3_exercise/day3_exercise_2.js
@@ -6,3 +6,12 @@ waitThenRun(function() {
         console.log('Goodbye!');
     }); // logs 'Goodbye!' 1.5 seconds later
 }); // logs 'Hello!' 1.5 seconds later
+
+// function that takes an argument
+function waitThenRun(callback) {
+    // use setTimeout, pass it a function and set the time in ms
+    setTimeout(function() {
+        //now use the argument we passed in the beginning, assume it's going to be a function and execute it
+        callback();
+    }, 1500);
+}
