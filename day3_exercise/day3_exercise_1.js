287f92d0e198e126fa59d9a5d68014d52b9eff96
David Durlan <david.a.durlan@gmail.com>
2019-11-20 12:19:43 +0100

    completed exercise

--- day3_exercise/day3_exercise_1.js
+++ day3_exercise/day3_exercise_1.js
@@ -6,3 +6,16 @@ sum(5, 10); //15
 sum(5, 10, 15); //30;
 
 sum(5, 10, 15, 100, 200); //330
+
+function sum() {
+    // declare the variable that holds the result
+    let result = 0;
+    //iterate trough the arguments array
+    for (let i = 0; i < arguments.length; i++) {
+        //add the current val to the var
+        result += arguments[i];
+    }
+    //log and return result
+    console.log(result);
+    return result;
+}
