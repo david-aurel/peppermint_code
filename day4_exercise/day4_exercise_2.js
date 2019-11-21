e95a5b5cefe0260a39c62ae47e6e147a8fea5a97
David Durlan <david.a.durlan@gmail.com>
2019-11-21 13:22:52 +0100

    completed exercise

--- day4_exercise/day4_exercise_2.js
+++ day4_exercise/day4_exercise_2.js
@@ -1,1 +1,6 @@
 // write a function that takes an array as a parameter and returns a new array containing all of the items that are in the array that was passed in but in reverse order. Unlike the reverse method that all arrays have, this function should leave the original array unchanged.
+
+function reverseArr(arr) {
+    // slice the arr to copy it, then reverse it
+    return arr.slice().reverse();
+}
