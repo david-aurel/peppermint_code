60af85bd0d2bd4bb43ea7d2fb34de43d5016d26b
David Durlan <david.a.durlan@gmail.com>
2019-11-21 16:22:43 +0100

    completed exercise

--- day4_exercise/day4_exercise_3.js
+++ day4_exercise/day4_exercise_3.js
@@ -1,5 +1,14 @@
 // Write a function called getLessThanZero that expects an array of numbers to be passed to it and returns a new array containing only those numbers from the array that was passed in that are less than zero.
 
+//declare getLessThanZero()
+function getLessThanZero(arr) {
+    //use filter to iterate trough the array
+    return arr.filter(function(num) {
+        // return when the current value is lower than 0
+        return num < 0;
+    });
+}
+
 // call getLessThanZero() and pass in arguments
 getLessThanZero([1, 2, -1, -90, 10]); //[-1, -90]
 getLessThanZero([1, 2]); //[]
