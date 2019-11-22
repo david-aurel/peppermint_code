7e989206038b1657908db114695ea296c06737bb
David Durlan <david.a.durlan@gmail.com>
2019-11-22 14:51:14 +0100

    completed exercise

--- day5_exercise/day5_exercise_1.js
+++ day5_exercise/day5_exercise_1.js
@@ -1,19 +1,21 @@
 // Write a constructor called Rectangle that accepts two numbers (width and height) as parameters. Rectangle instances should have a method called getArea that returns the instance's width multiplied by its height. Write another constructor called Square that accepts one number (which will serve as both width and the height) as a parameter. Instances of Square should also have a getArea method but you should not rewrite the getArea function you wrote for Rectangle. Square instances should use the same getArea method that Rectangle instances do.
+
 function Rectangle(w, h) {
     this.width = w;
     this.height = h;
+    this.getArea = function() {
+        return this.width * this.height;
+    };
 }
 
-function Square(w, h) {
-    this.width = w;
-    this.height = h;
+function Square(w) {
+    // 'this' refers to Rectangle's properties. 'w, w' has to be used because Rectangle expects two arguments
+    Rectangle.call(this, w, w);
 }
 
-return this.width * this.height; //only write this line once
-
-//test
-var square = new Square(4);
-square.getArea(); //16
-
+// Test
 var rect = new Rectangle(4, 5);
 rect.getArea(); //20
+
+var square = new Square(4);
+square.getArea(); //16
