484ac8e852b4850faf9f089a7a35c6bcf789337f
David Durlan <david.a.durlan@gmail.com>
2019-11-22 12:05:46 +0100

    add exercise task

---
+++ day5_exercise/day5_exercise_2.js
@@ -0,0 +1,10 @@
+// Write a function called invertCase that expects a string as a parameter. This function should return a new string with all the same characters as the string that was passed in but with the cases of the alphabetic characters switched. Uppercase characters should become lowercase and lowercase letters should become uppercase. Characters that are not alphabetic should not change. The toUpperCase and toLowerCase methods that all strings have will come in handy here.
+
+.toUpperCase()
+.toLowerCase()
+
+//loop through all the characters in the strings
+
+//apply toUpperCase or toLowerCase
+
+//return new string
