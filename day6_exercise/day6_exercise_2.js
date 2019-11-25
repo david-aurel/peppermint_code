faa30362a8d3b1095f9fdf059d629c6fad8c6f22
David Durlan <david.a.durlan@gmail.com>
2019-11-25 12:37:22 +0100

    completed exercise

--- day6_exercise/day6_exercise_2.js
+++ day6_exercise/day6_exercise_2.js
@@ -1,1 +1,14 @@
 // Write a function that expects a string representing a class name to be passed as a parameter. The function should return an array containing all the elements in the document that have the class that was passed in.
+
+function selectClass(str) {
+    // declare empty array and select all elements
+    var arr = [];
+    var elements = document.querySelectorAll('.' + str);
+    //loop trough all elements
+    elements.forEach(function(element) {
+        //push them to the array
+        arr.push(element);
+    });
+
+    return arr;
+}
