d744d5bf46b0e186f1d3001c62babd76b6538221
David Durlan <david.a.durlan@gmail.com>
2019-11-25 12:03:22 +0100

    set up exercise structure

---
+++ day6_exercise/day6_exercise_2.js
@@ -0,0 +1,1 @@
+// Write a function that expects a string representing a class name to be passed as a parameter. The function should return an array containing all the elements in the document that have the class that was passed in.
