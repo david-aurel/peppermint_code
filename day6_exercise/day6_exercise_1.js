59d7f95f82ab457b2a87eeddab773a42412de7bf
David Durlan <david.a.durlan@gmail.com>
2019-11-25 12:19:39 +0100

    completed exercise

--- day6_exercise/day6_exercise_1.js
+++ day6_exercise/day6_exercise_1.js
@@ -1,1 +1,13 @@
 // Write a function that expects a string representing a selector to be passed as a parameter. The function should find all the elements in the document that match the selector and change their style so that the text they contain is italic, underlined, and bold.
+
+function changeFont(str) {
+    //select all elements
+    var elements = document.querySelectorAll(str);
+    //iterate trough the collection you get back
+    for (i in elements) {
+        //change style
+        elements[i].style.fontStyle = 'italic';
+        elements[i].style.fontWeight = 'bold';
+        elements[i].style.textDecoration = 'underline';
+    }
+}
