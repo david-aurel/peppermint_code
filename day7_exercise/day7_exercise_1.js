a089e315ed49e35cd294174e394e5a1e62c2c641
David Durlan <david.a.durlan@gmail.com>
2019-11-26 13:29:07 +0100

    refactored

--- day7_exercise/day7_exercise_1.js
+++ day7_exercise/day7_exercise_1.js
@@ -4,7 +4,7 @@
 var square = document.querySelector('.mouseFollow');
 
 //add eventlistener when the mouse moves on the body, set the x and y location equal to the top and left of the square subtracting half of the squares height and width to make it centered
-document.body.addEventListener('mousemove', function(event) {
+document.addEventListener('mousemove', function(event) {
     square.style.top = event.pageY - 50 + 'px';
     square.style.left = event.pageX - 50 + 'px';
 });
