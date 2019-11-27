2e907a8281b74b171349af00afafe557ad4e3936
David Durlan <david.a.durlan@gmail.com>
2019-11-27 11:28:32 +0100

    add basic functionality

--- exercises/kitty_carousel/script.js
+++ exercises/kitty_carousel/script.js
@@ -4,13 +4,19 @@
 
     function moveSheep() {
         //add the offscren-left class to the onscreen sheep
-
-        //remove the onscreen class tfrom the onscreen sheep
-
+        sheep[current].classList.add('offscreen-left');
+        //remove the onscreen class from the onscreen sheep
+        sheep[current].classList.remove('onscreen');
         //figure out the next sheep
-
+        current++;
+        if (current >= sheep.length) {
+            current = 0;
+        }
         //add the onscreen class tothe next sheep
+        sheep[current].classList.add('onscreen');
 
-        setTimeOut(moveSheep, 5000);
+        setTimeout(moveSheep, 3000);
     }
+
+    moveSheep();
 })();
