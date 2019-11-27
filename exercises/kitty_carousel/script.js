48faddd66cb4deddf8391eda3b140ce4cbcda8b1
David Durlan <david.a.durlan@gmail.com>
2019-11-27 14:06:16 +0100

    added responsiveness

--- exercises/kitty_carousel/script.js
+++ exercises/kitty_carousel/script.js
@@ -1,6 +1,15 @@
 (() => {
     var sheep = document.querySelectorAll('#sheep img'),
+        circles = document.querySelectorAll('#circles div'),
         current = 0;
+
+    // //remove the offscreen-left class from the sheep that has it as soon as the animation is over
+    document.addEventListener('transitionend', function(e) {
+        if (e.target.classList.contains('offscreen-left')) {
+            e.target.classList.remove('offscreen-left');
+            setTimeout(moveSheep, 1000);
+        }
+    });
 
     function moveSheep() {
         //add the offscren-left class to the onscreen sheep
@@ -12,11 +21,13 @@
         if (current >= sheep.length) {
             current = 0;
         }
-        //add the onscreen class tothe next sheep
+        //add the onscreen class to the next sheep
         sheep[current].classList.add('onscreen');
-
-        setTimeout(moveSheep, 3000);
     }
 
     moveSheep();
+
+    function fillCircles() {
+        circles[current].
+    }
 })();
