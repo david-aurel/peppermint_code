39430f2a620228ec8d1a8074291215dcad7487e6
David Durlan <david.a.durlan@gmail.com>
2019-11-27 17:17:13 +0100

    add arrows and functionality to the right arrow

--- exercises/kitty_carousel/script.js
+++ exercises/kitty_carousel/script.js
@@ -1,11 +1,13 @@
 (() => {
     var sheep = document.querySelectorAll('#sheep img'),
         dots = document.querySelectorAll('.dot'),
+        arrows = document.querySelectorAll('.arrow'),
         current = 0,
         timer,
-        animation;
+        animation,
+        nextSlide;
 
-    // //remove the offscreen-left class from the sheep that has it as soon as the animation is over
+    // //remove the offscreen-left class from the sheep that has it as soon as the animation is over, i.e. moving it over to the right. Call the moveSlide() again.
     document.addEventListener('transitionend', function(e) {
         if (e.target.classList.contains('offscreen-left')) {
             e.target.classList.remove('offscreen-left');
@@ -19,10 +21,12 @@
 
         //add the offscren-left class to the onscreen sheep
         sheep[current].classList.add('offscreen-left');
-        //remove the onscreen class from the onscreen sheep
+
+        //remove the onscreen class from the onscreen sheep and the highlighted from the dot
         dots[current].classList.remove('highlighted');
         sheep[current].classList.remove('onscreen');
-        //figure out the next sheep
+
+        //figure out the next Slide
         if (typeof nextSlide == 'undefined') {
             current++;
             if (current >= sheep.length) {
@@ -31,14 +35,16 @@
         } else {
             current = nextSlide;
         }
-        //add the onscreen class to the next sheep
+
+        //add the onscreen class to the next Slide and the highlighted class to the dot
         dots[current].classList.add('highlighted');
         sheep[current].classList.add('onscreen');
     }
 
-    // set moveSheep
+    // fire up moveSlide with a delay of 2s on page load
     timer = setTimeout(moveSlide, 2000);
 
+    // create eventListeners for each dot, test if the right image is already onscreen or an animation is going on, in which case we do nothing. Stop the moveSlides() with clearTimeout and create another loop to get a correct value of i (scope issues), test which dot got clicked on and call moveSlide() again with the new Slide
     for (var i = 0; i < dots.length; i++) {
         dots[i].addEventListener('click', function(e) {
             if (e.target.classList.contains('highlighted')) {
@@ -50,8 +56,7 @@
             clearTimeout(timer);
             for (var i = 0; i < dots.length; i++) {
                 if (dots[i] == e.target) {
-                    var nextSlide = i;
-                    console.log(i);
+                    nextSlide = i;
                     moveSlide(nextSlide);
                     break;
                 }
@@ -59,10 +64,28 @@
         });
     }
 
-    //refactored version
+    //refactored version of the dot functionality
     // [].slice.call(dots).forEach(function(item, index) {
     //     item.addEventListener('click', function(e) {
     //         console.log(index);
     //     });
     // });
+
+    // add functionality to the right arrow, similar to the dots
+    for (var j = 0; j < arrows.length; j++) {
+        arrows[j].addEventListener('click', function(e) {
+            clearTimeout(timer);
+            if (animation) {
+                return;
+            }
+            if (e.target.classList.contains('fa-chevron-right')) {
+                if (current >= sheep.length) {
+                    nextSlide = 0;
+                    moveSlide(nextSlide);
+                } else {
+                    moveSlide(nextSlide);
+                }
+            }
+        });
+    }
 })();
