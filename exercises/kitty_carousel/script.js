5351ada31f4bafcfa8666f9309b8a2606f053037
David Durlan <david.a.durlan@gmail.com>
2019-11-27 16:21:47 +0100

    add working dots

--- exercises/kitty_carousel/script.js
+++ exercises/kitty_carousel/script.js
@@ -1,33 +1,68 @@
 (() => {
     var sheep = document.querySelectorAll('#sheep img'),
-        circles = document.querySelectorAll('#circles div'),
-        current = 0;
+        dots = document.querySelectorAll('.dot'),
+        current = 0,
+        timer,
+        animation;
 
     // //remove the offscreen-left class from the sheep that has it as soon as the animation is over
     document.addEventListener('transitionend', function(e) {
         if (e.target.classList.contains('offscreen-left')) {
             e.target.classList.remove('offscreen-left');
-            setTimeout(moveSheep, 1000);
+            timer = setTimeout(moveSlide, 2000);
+            animation = false;
         }
     });
 
-    function moveSheep() {
+    function moveSlide(nextSlide) {
+        animation = true;
+
         //add the offscren-left class to the onscreen sheep
         sheep[current].classList.add('offscreen-left');
         //remove the onscreen class from the onscreen sheep
+        dots[current].classList.remove('highlighted');
         sheep[current].classList.remove('onscreen');
         //figure out the next sheep
-        current++;
-        if (current >= sheep.length) {
-            current = 0;
+        if (typeof nextSlide == 'undefined') {
+            current++;
+            if (current >= sheep.length) {
+                current = 0;
+            }
+        } else {
+            current = nextSlide;
         }
         //add the onscreen class to the next sheep
+        dots[current].classList.add('highlighted');
         sheep[current].classList.add('onscreen');
     }
 
-    moveSheep();
+    // set moveSheep
+    timer = setTimeout(moveSlide, 2000);
 
-    function fillCircles() {
-        circles[current].
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].addEventListener('click', function(e) {
+            if (e.target.classList.contains('highlighted')) {
+                return;
+            }
+            if (animation) {
+                return;
+            }
+            clearTimeout(timer);
+            for (var i = 0; i < dots.length; i++) {
+                if (dots[i] == e.target) {
+                    var nextSlide = i;
+                    console.log(i);
+                    moveSlide(nextSlide);
+                    break;
+                }
+            }
+        });
     }
+
+    //refactored version
+    // [].slice.call(dots).forEach(function(item, index) {
+    //     item.addEventListener('click', function(e) {
+    //         console.log(index);
+    //     });
+    // });
 })();
