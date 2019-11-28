63e51cfd073af8158090a5a1e9422ef03f8a75e7
David Durlan <david.a.durlan@gmail.com>
2019-11-28 14:50:57 +0100

    add man on canvas

--- exercises/08_canvas/script.js
+++ exercises/08_canvas/script.js
@@ -1,11 +1,29 @@
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
 
+//man head
 ctx.beginPath();
 ctx.lineWidth = 3;
 ctx.arc(165, 215, 50, 0, 2 * Math.PI); //arc(x, y, radius, startAngle, endAngle)
 ctx.stroke();
 
+//man arm
 ctx.beginPath();
-ctx.arc(165, 215, 100, 0 * Math.PI, 0.5 * Math.PI); //arc(x, y, radius, startAngle, endAngle)
+ctx.arc(170, 195, 100, 0.05 * Math.PI, 0.5 * Math.PI); //arc(x, y, radius, startAngle, endAngle)
+ctx.stroke();
+
+//man body and leg
+ctx.beginPath();
+ctx.moveTo(165, 265);
+ctx.lineTo(185, 420);
+ctx.lineTo(80, 420);
+ctx.lineTo(70, 435);
+ctx.stroke();
+
+//man second leg
+ctx.beginPath();
+ctx.moveTo(175, 350);
+ctx.lineTo(235, 350);
+ctx.lineTo(245, 425);
+ctx.lineTo(265, 430);
 ctx.stroke();
