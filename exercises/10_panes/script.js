f26366b3e0e45bfb64a3d237efb640c55ff0df05
David Durlan <david.a.durlan@gmail.com>
2019-12-02 16:28:29 +0100

    update comments

--- exercises/10_panes/script.js
+++ exercises/10_panes/script.js
@@ -3,8 +3,9 @@ var container = $('#container'),
     topImage = $('#top-image'),
     slider = $('#slider');
 
-//when the cursor enters the slider, adjust the left of the top image and the left of the slider
+//when the cursor moves inside the container, the cursor is clicking down and is not leaving the container, adjust the left of the top image and the left of the slider
 
+// feels like a huge workaround to a simple thing I cant figure out. Needs refactoring
 container.on('mousemove', function(e) {
     var x = e.clientX - container.offset().left - 10;
     if (e.buttons === 1) {
