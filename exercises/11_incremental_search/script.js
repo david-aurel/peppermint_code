cde88da76fd495e6538e6d9022dadbd76aa15c0d
David Durlan <david.a.durlan@gmail.com>
2019-12-03 10:19:09 +0100

    first commit

---
+++ exercises/11_incremental_search/script.js
@@ -0,0 +1,20 @@
+// 1. input event
+// get a list of countries that match the input we get from the user
+// if the user types gibberish, show a message that says 'no results'
+
+// 2. mouseover event
+// i.e. hover - add a highlight class to the country we are currently hovering over
+// remove highlight class from country we are not hovering over
+
+// 3. mousedown event
+// take the country the user is clicking on and put it in the input field
+
+// 4. key event
+// allow user to scroll through the list using the up and down arrows
+// when I press enter, that country should appear on the input field
+
+// 5. blur event
+// we want to hide th results
+
+// 6. focus event
+// we want to show the results
