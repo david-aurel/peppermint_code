e31f99323e8fdcf38c8e75f59e013df81db5cc89
David Durlan <david.a.durlan@gmail.com>
2019-12-09 21:26:40 +0100

    refactor everything, add more button logic

--- projects/spotify_search/script.js
+++ projects/spotify_search/script.js
@@ -1,12 +1,12 @@
 (() => {
     $('#submit-button').on('click', function() {
-        var userInput = $('input[name=user-input').val();
-        // console.log('user-input: ', userInput);
-        var albumOrArtist = $('select').val();
-        // console.log('albumOrArtist: ', albumOrArtist);
+        var userInput = $('input[name=user-input').val(),
+            albumOrArtist = $('select').val(),
+            startUrl = 'https://elegant-croissant.glitch.me/spotify',
+            imageUrl = 'default.png';
 
         $.ajax({
-            url: 'https://elegant-croissant.glitch.me/spotify',
+            url: startUrl,
             method: 'GET',
             //any sort of data we need to send to the API so we get a response
             data: {
@@ -17,37 +17,63 @@
                 //this runs when we get a response from the API
                 response = response.artists || response.albums;
                 console.log('response: ', response);
-                if (response.items.length > 0) {
-                    $('#results-header').html(
-                        'Results for "' + userInput + '"'
-                    );
-                } else {
-                    $('#results-header').html('No results');
-                }
-                var myHtml = '';
-                for (var i = 0; i < response.items.length; i++) {
-                    // console.log(response.items[i].name);
 
-                    var imageUrl = 'default.png';
-                    if (response.items[i].images[0]) {
-                        imageUrl = response.items[i].images[0].url;
+                if ($('results-container').length == 0) {
+                    if (response.items.length > 0) {
+                        $('#results-header').html(
+                            'Results for "' + userInput + '"'
+                        );
+                        $('#more-button').removeClass('invisible');
+                    } else {
+                        $('#results-header').html('No results');
                     }
-                    myHtml +=
-                        '<div class="result"><img src="' +
-                        imageUrl +
-                        '">' +
-                        '<p>' +
-                        response.items[i].name +
-                        '</p></div>';
                 }
-                $('#results-container').html(myHtml);
 
-                var nextUrl =
-                    response.next &&
-                    response.next.replace(
-                        'https://api.spotify.com/v1/search',
-                        'https://elegant-croissant.glitch.me/spotify'
-                    );
+                var myHtml = '';
+
+                displayResults(response);
+                function displayResults(response) {
+                    for (var i = 0; i < response.items.length; i++) {
+                        if (response.items[i].images[0]) {
+                            imageUrl = response.items[i].images[0].url;
+                        }
+                        myHtml +=
+                            '<div class="result"><a href="' +
+                            response.items[i].external_urls.spotify +
+                            '".>' +
+                            '<img src="' +
+                            imageUrl +
+                            '">' +
+                            '<p>' +
+                            response.items[i].name +
+                            '</p></a></div>';
+                    }
+                    $('#results-container').html(myHtml);
+
+                    var nextUrl =
+                        response.next &&
+                        response.next.replace(
+                            'https://api.spotify.com/v1/search',
+                            'https://elegant-croissant.glitch.me/spotify'
+                        );
+                    $('#more-button').on('click', function() {
+                        $.ajax({
+                            url: nextUrl,
+                            method: 'GET',
+                            //any sort of data we need to send to the API so we get a response
+                            data: {
+                                query: userInput,
+                                type: $('select').val()
+                            },
+                            success: function(response) {
+                                response = response.artists || response.albums;
+                                console.log(response);
+
+                                displayResults(response);
+                            }
+                        });
+                    });
+                }
             }
         });
     });
