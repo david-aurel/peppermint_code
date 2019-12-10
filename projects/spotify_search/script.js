c229d1729e80375df73c5543a7329fe65330e3e3
David Durlan <david.a.durlan@gmail.com>
2019-12-10 11:13:49 +0100

    refactoring

--- projects/spotify_search/script.js
+++ projects/spotify_search/script.js
@@ -1,80 +1,77 @@
 (() => {
+    var imageUrl = 'default.png',
+        input = $('input[name=user-input'),
+        select = $('select'),
+        nextUrl = '',
+        myHtml = '';
+
     $('#submit-button').on('click', function() {
-        var userInput = $('input[name=user-input').val(),
-            albumOrArtist = $('select').val(),
-            startUrl = 'https://elegant-croissant.glitch.me/spotify',
-            imageUrl = 'default.png';
+        var url = 'https://elegant-croissant.glitch.me/spotify';
 
         $.ajax({
-            url: startUrl,
-            method: 'GET',
-            //any sort of data we need to send to the API so we get a response
+            url: url,
             data: {
-                query: userInput,
-                type: albumOrArtist
+                query: input.val(),
+                type: select.val()
             },
-            success: function(response) {
-                //this runs when we get a response from the API
-                response = response.artists || response.albums;
-                console.log('response: ', response);
+
+            success: function(data) {
+                data = data.artists || data.albums;
+                setNextUrl(data.next);
+                console.log('response: ', data);
 
                 if ($('results-container').length == 0) {
-                    if (response.items.length > 0) {
+                    if (data.items.length > 0) {
                         $('#results-header').html(
-                            'Results for "' + userInput + '"'
+                            'Results for "' + input.val() + '"'
                         );
                         $('#more-button').removeClass('invisible');
                     } else {
                         $('#results-header').html('No results');
                     }
                 }
-
-                var myHtml = '';
-
-                displayResults(response);
-                function displayResults(response) {
-                    for (var i = 0; i < response.items.length; i++) {
-                        if (response.items[i].images[0]) {
-                            imageUrl = response.items[i].images[0].url;
-                        }
-                        myHtml +=
-                            '<div class="result"><a href="' +
-                            response.items[i].external_urls.spotify +
-                            '".>' +
-                            '<img src="' +
-                            imageUrl +
-                            '">' +
-                            '<p>' +
-                            response.items[i].name +
-                            '</p></a></div>';
-                    }
-                    $('#results-container').html(myHtml);
-
-                    var nextUrl =
-                        response.next &&
-                        response.next.replace(
-                            'https://api.spotify.com/v1/search',
-                            'https://elegant-croissant.glitch.me/spotify'
-                        );
-                    $('#more-button').on('click', function() {
-                        $.ajax({
-                            url: nextUrl,
-                            method: 'GET',
-                            //any sort of data we need to send to the API so we get a response
-                            data: {
-                                query: userInput,
-                                type: $('select').val()
-                            },
-                            success: function(response) {
-                                response = response.artists || response.albums;
-                                console.log(response);
-
-                                displayResults(response);
-                            }
-                        });
-                    });
-                }
+                getResultsHtml(data);
             }
         });
     });
+
+    $(document).on('click', '#more-button', function() {
+        $.ajax({
+            url: nextUrl,
+            success: function(data) {
+                data = data.artists || data.albums;
+                setNextUrl(data.next);
+                console.log(data);
+                getResultsHtml(data);
+            }
+        });
+    });
+
+    function setNextUrl(next) {
+        nextUrl =
+            next &&
+            next.replace(
+                'https://api.spotify.com/v1/search',
+                'https://elegant-croissant.glitch.me/spotify'
+            );
+    }
+
+    function getResultsHtml(data) {
+        for (var i = 0; i < data.items.length; i++) {
+            if (data.items[i].images[0]) {
+                imageUrl = data.items[i].images[0].url;
+            }
+            myHtml +=
+                '<div class="result"><a href="' +
+                data.items[i].external_urls.spotify +
+                '".>' +
+                '<img src="' +
+                imageUrl +
+                '">' +
+                '<p>' +
+                data.items[i].name +
+                '</p></a></div>';
+        }
+        $('#results-container').append(myHtml);
+    }
 })();
