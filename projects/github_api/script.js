63354d08cff3d9c9d0e9ab459f318bb5bd25bfea
David Durlan <david.a.durlan@gmail.com>
2019-12-11 10:48:42 +0100

    define variables to store user input

---
+++ projects/github_api/script.js
@@ -0,0 +1,7 @@
+(() => {
+    $('button').on('click', function() {
+        var username = $('input[name="username"]').val();
+        var password = $('input[name="password"]').val();
+        var userToSearch = $('input[name="user-to-search"]').val();
+    });
+})();
