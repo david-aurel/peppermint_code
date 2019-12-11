76b798ec5d828e1e7f9f571032e970fad3877bf7
David Durlan <david.a.durlan@gmail.com>
2019-12-11 16:11:30 +0100

    add toggling the commits results

--- projects/github_api/script.js
+++ projects/github_api/script.js
@@ -34,24 +34,34 @@
     $('.repos-container').on('click', '.card', function() {
         var element = $(this);
         var elementId = element.attr('id');
+        var contentOfId = $(`#${elementId} .content`);
         var username = $('input[name="username"]').val();
         var password = $('input[name="password"]').val();
         var userToSearch = $('input[name="user-to-search"]').val();
         var baseUrl = 'https://api.github.com';
         var endpoint = '/repos/' + userToSearch + '/' + elementId + '/commits';
-        $.ajax({
-            url: baseUrl + endpoint,
-            headers: {
-                authorization: 'Basic ' + btoa(username + ':' + password)
-            },
-            success: function(response) {
-                response = response.slice(0, 10);
 
-                var commitsTemplate = Handlebars.templates.commits({
-                    data: response
-                });
-                $('#' + elementId).append(commitsTemplate);
+        if (contentOfId.length == 0) {
+            $.ajax({
+                url: baseUrl + endpoint,
+                headers: {
+                    authorization: 'Basic ' + btoa(username + ':' + password)
+                },
+                success: function(response) {
+                    response = response.slice(0, 10);
+
+                    var commitsTemplate = Handlebars.templates.commits({
+                        data: response
+                    });
+                    $('#' + elementId).append(commitsTemplate);
+                }
+            });
+        } else {
+            if (contentOfId.hasClass('visible')) {
+                contentOfId.removeClass('visible');
+            } else {
+                contentOfId.addClass('visible');
             }
-        });
+        }
     });
 })();
