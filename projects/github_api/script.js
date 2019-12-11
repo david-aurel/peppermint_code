4b60fafa100dd75994d939aa4ba4e0a178f7d25e
David Durlan <david.a.durlan@gmail.com>
2019-12-11 11:27:22 +0100

    add ajax request

--- projects/github_api/script.js
+++ projects/github_api/script.js
@@ -1,7 +1,29 @@
 (() => {
+    ///////////// HANDLEBAR  /////////////
+    Handlebars.templates = Handlebars.templates || {};
+    var templates = document.querySelectorAll(
+        'script[type="text/x-handlebars-template"]'
+    );
+    Array.prototype.slice.call(templates).forEach(function(script) {
+        Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
+    });
+    ///////////// HANDLEBAR  /////////////
+
     $('button').on('click', function() {
         var username = $('input[name="username"]').val();
         var password = $('input[name="password"]').val();
         var userToSearch = $('input[name="user-to-search"]').val();
+        var baseUrl = 'https://api.github.com';
+        var endpoint = '/users/' + userToSearch + '/repos';
+
+        $.ajax({
+            url: baseUrl + endpoint,
+            headers: {
+                authorization: 'Basic ' + btoa(username + ':' + password)
+            },
+            success: function(data) {
+                console.log(data);
+            }
+        });
     });
 })();
