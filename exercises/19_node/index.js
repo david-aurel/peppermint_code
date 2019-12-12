8541be40a83c55d9503386b5a620223bead012be
David Durlan <david.a.durlan@gmail.com>
2019-12-12 13:10:35 +0100

    refactor query parameters

--- exercises/19_node/index.js
+++ exercises/19_node/index.js
@@ -5,7 +5,7 @@ const querystring = require('querystring');
 
 let { protocol, host, hostname, port, pathname, query } = url.parse(input);
 let queryObj = querystring.parse(query);
-let queryObjKeys = Object.keys(queryObj);
+let queryArr = Object.entries(queryObj);
 
 console.log('the protocol is', protocol);
 console.log('the host is', host);
@@ -15,11 +15,9 @@ console.log('the pathname is', pathname);
 console.log('the query is', query);
 
 if (query) {
-    for (let i = 0; i < Object.keys(queryObj).length; i++) {
+    for (const i in queryArr) {
         console.log(
-            `the value of the ${Object.keys(queryObj)[i]} parameter is ${
-                queryObj[Object.keys(queryObj)[i]]
-            } `
+            `the value of the ${queryArr[i][0]} parameter is ${queryArr[i][1]}`
         );
     }
 }
