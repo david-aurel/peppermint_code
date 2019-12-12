5da081c8834142535305665727d82dff0af6f86c
David Durlan <david.a.durlan@gmail.com>
2019-12-12 16:40:52 +0100

    done

--- exercises/19_text_adventure/index.js
+++ exercises/19_text_adventure/index.js
@@ -1,32 +1,62 @@
 const readline = require('readline'),
-    chalk = require('chalk'),
-    italic = chalk.italic;
+    chalk = require('chalk');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-const conversation = {
-    q: 'Is it friday yet? ',
+let color = chalk.italic;
+
+const story = {
+    q: 'Welcome to The Land Of Wizards! Would you like to play? [yes/no] ',
     a: {
         yes: {
-            q: 'Great! What are your weekend plans? '
+            q:
+                'You are alone in a dark forest and facing a fork in the road. Which direction do you turn? [left/ right] ',
+            a: {
+                left: {
+                    q:
+                        "There's a scary wizard! He asks you a tough question. What's 1+1? ",
+                    a: {
+                        '2': 'congratulations!'
+                    }
+                },
+                right: 'This was not the right choice. Goodbye!'
+            }
         },
-        no: 'Too bad... bye!'
+        no: 'Alright then. Enjoy your day!'
     }
 };
 
 function askQuestion(obj) {
-    rl.question(italic(obj.q), answer => {
-        if (typeof obj.a[answer] === 'object') {
-            console.log(italic('awwww...'));
-            rl.close();
+    rl.question(color(obj.q), answer => {
+        if (typeof obj.a[answer] == 'object') {
+            obj = obj.a[answer];
+
+            rl.question(color(obj.q), answer => {
+                if (typeof obj.a[answer] == 'object') {
+                    obj = obj.a[answer];
+
+                    rl.question(color(obj.q), answer => {
+                        if (obj.a[answer]) {
+                            console.log(obj.a[answer]);
+                            rl.close();
+                        } else {
+                            console.log(obj.a.yes.a['right']);
+                            rl.close();
+                        }
+                    });
+                } else {
+                    console.log(obj.a['right']);
+                    rl.close();
+                }
+            });
         } else {
-            console.log(`that's not the right answer... try again!`);
-            askQuestion(obj);
+            console.log(obj.a['no']);
+            rl.close();
         }
     });
 }
 
-askQuestion(conversation);
+askQuestion(story);
