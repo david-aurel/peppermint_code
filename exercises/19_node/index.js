b832dc922ecfbf61c7ab0bba1ff7dc83180685bc
David Durlan <david.a.durlan@gmail.com>
2019-12-12 13:14:49 +0100

    make logging the url parts a function

--- exercises/19_node/index.js
+++ exercises/19_node/index.js
@@ -1,23 +1,24 @@
-let input = process.argv[2];
+const input = process.argv[2],
+    url = require('url'),
+    querystring = require('querystring'),
+    { protocol, host, hostname, port, pathname, query } = url.parse(input),
+    queryObj = querystring.parse(query),
+    queryArr = Object.entries(queryObj);
 
-const url = require('url');
-const querystring = require('querystring');
+logUrlParts();
 
-let { protocol, host, hostname, port, pathname, query } = url.parse(input);
-let queryObj = querystring.parse(query);
-let queryArr = Object.entries(queryObj);
-
-console.log('the protocol is', protocol);
-console.log('the host is', host);
-console.log('the hostname is', hostname);
-console.log('the port is ', port);
-console.log('the pathname is', pathname);
-console.log('the query is', query);
-
-if (query) {
-    for (const i in queryArr) {
-        console.log(
-            `the value of the ${queryArr[i][0]} parameter is ${queryArr[i][1]}`
-        );
+function logUrlParts() {
+    console.log('the protocol is', protocol);
+    console.log('the host is', host);
+    console.log('the hostname is', hostname);
+    console.log('the port is ', port);
+    console.log('the pathname is', pathname);
+    console.log('the query is', query);
+    if (query) {
+        for (const i in queryArr) {
+            console.log(
+                `the value of the ${queryArr[i][0]} parameter is ${queryArr[i][1]}`
+            );
+        }
     }
 }
