9a510d45ba436119c209a6720e24579daa8b5a6b
David Durlan <david.a.durlan@gmail.com>
2019-12-13 12:04:40 +0100

    get started on part 1

--- exercises/20_fs/fun-with-fs/index.js
+++ exercises/20_fs/fun-with-fs/index.js
@@ -1,1 +1,17 @@
-const fs = require('fs');
+const fs = require('fs'),
+    myPath = __dirname,
+    filesPath = myPath + '/files';
+
+// Part 1
+
+function logSizes(path) {
+    fs.readdir(path, { withFileTypes: true }, (err, files) => {
+        if (err) {
+            console.log('error in readdir: ', err);
+        } else {
+            console.log(files);
+        }
+    });
+}
+
+logSizes(filesPath);
