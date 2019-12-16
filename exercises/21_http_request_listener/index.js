d66bba16877e5a0e2f55e55805c31cea8b47d1ae
David Durlan <david.a.durlan@gmail.com>
2019-12-16 15:22:48 +0100

    set up handling requests

--- exercises/21_http_request_listener/index.js
+++ exercises/21_http_request_listener/index.js
@@ -1,15 +1,25 @@
 const http = require('http');
 
-const server = http.createServer((request, response) => {
-    let body = [];
-    request
-        .on('data', chunk => {
-            body.push(chunk);
-        })
-        .on('end', () => {
-            body = Buffer.concat(body).toString();
+const server = http.createServer((req, res) => {
+    req.on('error', err => console.log('req err', err));
+    res.on('error', err => console.log('req err', err));
+
+    if (req.method === 'GET') {
+        res.write(`<p>Hello Get</p>`)
+        res.end()
+    }
+    if (req.method === 'POST') {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk;
         });
-});
+
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+
+        res.write(`<p>Hello Post</p>`);
+    }
 
 server.listen(8080, () => {
     console.log('server is running');
