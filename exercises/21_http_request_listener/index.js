df67dd4089a834d00d9d9d4df7e47b0d584d3e5f
David Durlan <david.a.durlan@gmail.com>
2019-12-16 15:59:27 +0100

    add request handlers for get, head, post and everything else, exercise done

--- exercises/21_http_request_listener/index.js
+++ exercises/21_http_request_listener/index.js
@@ -3,32 +3,37 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     req.on('error', err => console.log('req err', err));
     res.on('error', err => console.log('req err', err));
+    console.log(
+        `req method: '${req.method}', req url: '${req.url}', req header:,${req.headers}`
+    );
 
     if (req.method === 'GET') {
-        console.log(
-            `req method: '${req.method}', req url: '${req.url}', req header:`,
-            req.headers
-        );
-        res.write(`<p>Hello Get</p>`);
         res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<!doctype html>
+                    <html>
+                    <title>Hello World!</title>
+                    <p>Hello World!
+                    </html>`);
         res.end();
-    }
-
-    if (req.method === 'POST') {
+    } else if (req.method === 'HEAD') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end();
+    } else if (req.method === 'POST') {
         let body = '';
-
         req.on('data', chunk => {
             body += chunk;
         });
-
         req.on('end', () => {
             console.log(('body: ', body));
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            res.end();
         });
-
-        res.setHeader('Content-Type', 'text/html');
-        res.statusCode = 200;
-
-        res.write(`<p>Hello Post</p>`);
+    } else {
+        res.statusCode = 405;
+        res.end();
     }
 });
 
