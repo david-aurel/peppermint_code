56ec9b179f5437579009c39c143119763092c3ee
David Durlan <david.a.durlan@gmail.com>
2019-12-17 10:32:28 +0100

    set up basic server

--- projects/portfolio/portfolio_app.js
+++ projects/portfolio/portfolio_app.js
@@ -1,5 +1,16 @@
-const http =require('http');
-http.createServer((req, res) => {
+// require modules
+const http = require('http'),
+    fs = require('fs');
 
-}).listen(8080, () => console.log('portfolio server is listening on port 8080'));
-)
+http.createServer((req, res) => {
+    // handling errors
+    req.on('error', () => console.log('error in request'));
+    res.on('error', () => console.log('error in response'));
+
+    const readStream = fs.createReadStream(
+        `${__dirname}/../connect_four/index.html`
+    );
+    readStream.pipe(res);
+}).listen(8080, () =>
+    console.log('portfolio server is listening on port 8080')
+);
