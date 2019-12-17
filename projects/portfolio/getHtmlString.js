b9e694dbdfd40def329b0e340160d24498df37dc
David Durlan <david.a.durlan@gmail.com>
2019-12-17 14:41:16 +0100

    create html string for all projects in directory

--- projects/portfolio/getHtmlString.js
+++ projects/portfolio/getHtmlString.js
@@ -1,3 +1,30 @@
-module.exports = function hello(name) {
-    console.log('hi', name);
-};
+const fs = require('fs');
+
+module.exports = getHtmlString;
+
+function getHtmlString(path) {
+    let files = fs.readdirSync(path);
+
+    let html = `<!DOCTYPE html>
+                        <html lang="en" dir="ltr">
+                        <head>
+                            <meta charset="utf-8">
+                            <title>Portfolio</title>
+                        </head>
+                        <body>
+                            <h1>Portfolio</h1>
+                            <h4>Projects:</h4>
+                            <ul>`;
+
+    for (let i in files) {
+        if (files[i] == 'portfolio') {
+            html += `<li><a href="/">${files[i]}</a></li>`;
+        } else {
+            html += `<li><a href="/${files[i]}">${files[i]}</a></li>`;
+        }
+    }
+    html += `       </ul> 
+                        </body>
+                    </html>`;
+    return html;
+}
