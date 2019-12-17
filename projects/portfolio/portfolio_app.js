62903093fe88062fb780d428849afd5796c0fa58
David Durlan <david.a.durlan@gmail.com>
2019-12-17 11:06:36 +0100

    add protection against traversal attacks

--- projects/portfolio/portfolio_app.js
+++ projects/portfolio/portfolio_app.js
@@ -1,6 +1,7 @@
 // require modules
 const http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
 http.createServer((req, res) => {
     // handling errors
@@ -8,9 +9,30 @@ http.createServer((req, res) => {
     res.on('error', () => console.log('error in response'));
 
     const readStream = fs.createReadStream(
-        `${__dirname}/../connect_four/index.html`
+        `${__dirname}/../../projects/connect_four/index.html`
     );
     readStream.pipe(res);
+
+    if (req.method != 'GET') {
+        res.statusCode = 405;
+        return res.end();
+    }
+    console.log(
+        'normalized path: ',
+        path.normalize(`../../projects/connect_four/index.html`)
+    );
+
+    const filePath = `${__dirname}/../../projects/${req.url}`;
+
+    if (
+        !path
+            .normalize(filePath)
+            .startsWith(`/Users/David/Desktop/Code/peppermint-code/projects/`)
+    ) {
+        res.statusCode = 403;
+        console.log('intruder!!!');
+        return res.end();
+    }
 }).listen(8080, () =>
     console.log('portfolio server is listening on port 8080')
 );
