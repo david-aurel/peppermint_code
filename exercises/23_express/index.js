4c4ac691931b1b95ec5e532d675156c0486341fc
David Durlan <david.a.durlan@gmail.com>
2019-12-18 15:52:13 +0100

    add cookies

--- exercises/23_express/index.js
+++ exercises/23_express/index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-    app = express();
+    app = express(),
+    cookieParser = require('cookie-parser');
 
 // middleware function that grabs user input, parses it and puts it into a js object
 app.use(
@@ -8,15 +9,30 @@ app.use(
     })
 );
 
+// our own middleware -  this will run every time my server receives a request
+app.use((req, res, next) => {
+    console.log('our very own middleware is running');
+    console.log('req.url:', req.url);
+    next();
+});
+
+// serves all of our static files
+//whatever you pass to static is the name of your project directory
+app.use(express.static('./public'));
+
+app.use(cookieParser());
+
 // listens for a get request to the '/' slash route
 app.get('/', (req, res) => {
     console.log(`GET request to '/' happened!`);
+    console.log(`What cookies look like:`, req.cookies);
     res.send(`<h1>Express Demo</h1>`);
 });
 
 // sending a file
 app.get('/about', (req, res) => {
     console.log(`GET request to '/about' happened`);
+    res.cookie('authenticated', true);
     res.sendFile(__dirname + '/index.html');
 });
 
@@ -41,13 +57,24 @@ app.get('/add-cute-animal', (req, res) => {
             </form>`);
 });
 
-// rew.body gives us access to the user input - in order to see it we need to put the middleware function express.urlencoded on top
+// rew.body gives us access to the user input - in order to see it we need to add the middleware above!
 app.post('/add-cute-animal', (req, res) => {
     console.log(`POST Request to '/add-cute-animal' happened`);
     console.log(`req.body:`, req.body);
     res.send(
         `<p>The animal is <strong>${req.body.animal}</strong> and it's score is <strong>${req.body.score}</strong>.`
     );
+});
+
+app.get('/secret-page', (req, res) => {
+    console.log(`cookies in '/secrete-page':`, req.cookies);
+
+    if (req.cookies.authenticated) {
+        res.send(`<p>super secret page</p>`);
+    } else {
+        // sorry, you can't see my super secret page
+        res.redirect('/');
+    }
 });
 
 app.listen(8080, () => console.log(`my first express server is running!`));
