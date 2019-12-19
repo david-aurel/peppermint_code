eab6bca2d6b0059f9f566ce92a9e12b54ac193f4
David Durlan <david.a.durlan@gmail.com>
2019-12-19 13:58:26 +0100

    render home template

--- projects/portfolio/portfolio_app_express_handlebars.js
+++ projects/portfolio/portfolio_app_express_handlebars.js
@@ -1,3 +1,20 @@
+//require modules
 const express = require('express'),
     app = express(),
-    expressHandlebars = require('express-handlebars');
+    hb = require('express-handlebars');
+
+// this configures express to use express handlebars
+app.engine('handlebars', hb());
+app.set('view engine', 'handlebars');
+
+// serves static files
+app.use(express.static(__dirname + '/../'));
+
+app.listen(8080, () => console.log('listening...'));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        layout: null,
+        cohort: 'Peppermint'
+    });
+});
