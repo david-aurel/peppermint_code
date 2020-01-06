ba315af870836e6e48dd7a54e93394dc378b2d6e
David Durlan <david.a.durlan@gmail.com>
2020-01-06 11:23:59 +0100

    set up everything to start working on the exercise

--- projects/ticker_twitter/index.js
+++ projects/ticker_twitter/index.js
@@ -1,9 +1,26 @@
-const { getToken } = require('./twitter');
+const express = require('express'),
+    app = express(),
+    PORT = 8080;
 
-//function to log the token it was given
-function logToken(token) {
-    console.log('I need the function to do something...', token);
-}
+const { getToken } = require(`${__dirname}/twitter.js`);
 
-//calling getToken() and passing logToken() as a callback to run after getToken() is finished.
-getToken(logToken);
+app.use(express.static(`${__dirname}/ticker`));
+
+app.get('/links.json', (req, res) => {
+    console.log('a request was made for /links.json');
+    //1. We want to get a bearerToken from twitter
+    //2. When we have the token, make a request for tweets
+    //3. We then want to filter the data (make it look like our old links.json)
+    //4. Send back those filtered tweets as JSON
+});
+
+//FAKEN TOKEN BEHAVIOUR
+// //function to log the token it was given
+// function logToken(token) {
+//     console.log('I need the function to do something...', token);
+// }
+
+// //calling getToken() and passing logToken() as a callback to run after getToken() is finished.
+// getToken(logToken);
+
+app.listen(PORT, () => console.log('Twitter ticker listening on Port', PORT));
