b7f58020d87eae956d90129cd484c4f135c5f91e
David Durlan <david.a.durlan@gmail.com>
2020-01-06 12:20:04 +0100

    finish code along from class, start working on the exercise now

--- projects/ticker_twitter/index.js
+++ projects/ticker_twitter/index.js
@@ -2,19 +2,40 @@ const express = require('express'),
     app = express(),
     PORT = 8080;
 
-const { getToken } = require(`${__dirname}/twitter.js`);
+const {
+    getToken,
+    getTweets,
+    filterTweets
+} = require(`${__dirname}/twitter.js`);
 
 app.use(express.static(`${__dirname}/ticker`));
 
 app.get('/links.json', (req, res) => {
     console.log('a request was made for /links.json');
+
     //1. We want to get a bearerToken from twitter
-    //2. When we have the token, make a request for tweets
-    //3. We then want to filter the data (make it look like our old links.json)
-    //4. Send back those filtered tweets as JSON
+    getToken(function(err, bearerToken) {
+        if (err) {
+            console.log(`error in getToken(): ${err}`);
+            return;
+        }
+        //2. When we have the token, make a request for tweets
+        console.log(`in index.js, about to call getTweets(): ${bearerToken}`);
+
+        getTweets(bearerToken, function(err, tweets) {
+            if (err) {
+                console.log(`error in getTweets(): ${err}`);
+                return;
+            }
+        });
+        //3. We then want to filter the data (make it look like our old links.json)
+        const filteredTweets = filterTweets(tweets);
+        //4. Send back those filtered tweets as JSON
+        res.json(filteredTweets);
+    });
 });
 
-//FAKEN TOKEN BEHAVIOUR
+//FAKE TOKEN BEHAVIOUR
 // //function to log the token it was given
 // function logToken(token) {
 //     console.log('I need the function to do something...', token);
--- projects/ticker_twitter/twitter.js
+++ projects/ticker_twitter/twitter.js
@@ -1,10 +1,56 @@
-//export this function for other scripts to use
+const secrets = require(`${__dirname}/secrets`);
+const https = require('https');
+
 //getToken() takes a callback function as an argument. The callback will be logToken(). It will run, after getToken() is finished, because of the nature of callbacks.
 module.exports.getToken = function(callback) {
-    let token;
-    setTimeout(() => {
-        token = 'My-fake-token';
-        console.log('about to return token...');
-        callback(token);
-    }, 1000);
+    console.log('this function gets the bearerToken from twitter');
+
+    let creds = `${secrets.consumerKey}: ${secrets.consumerSecret}`,
+        encodedCreds = Buffer.from(creds).toString('base64');
+
+    const options = {
+        host: 'api.twitter.com',
+        path: '/oauth2/token',
+        method: 'POST',
+        headers: {
+            Authorization: `Basic ${encodedCreds}`,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            // 'Content-Lenght': ``,
+            // 'Accept-Encoding': 'gzip'
+        }
+    };
+
+    const cb = function(response) {
+        console.log(`response: ${response.statusCode}`);
+        if (response.statusCode !== 200) {
+            //this means something went wrong
+            callback(response.statusCode);
+            return;
+        }
+
+        var body = '';
+        response.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        response.on('end', function() {
+            console.log(`body: ${body}`);
+            let parsedBody = JSON.parse(body);
+            console.log(`parsedBody: ${parsedBody}`);
+
+            //null is the error, and then the access token
+            callback(null, parsedBody.access_token);
+        });
+    };
+
+    const req = https.request(options, cb);
+
+    req.end('grant_type=client_credentials');
+};
+
+module.exports.getTweets = function(bearerToken, callback) {
+    console.log('this function gets tweets from twitter api');
+};
+module.exports.filterTweets = function(tweets) {
+    console.log('this function filters the response from twitter');
 };
