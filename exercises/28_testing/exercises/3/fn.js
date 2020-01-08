039a7a983027aba0e62ef88595777029e4ce0c6e
David Durlan <david.a.durlan@gmail.com>
2020-01-08 19:01:23 +0100

    make 3.(fn) work

--- exercises/28_testing/exercises/3/fn.js
+++ exercises/28_testing/exercises/3/fn.js
@@ -1,2 +1,21 @@
-module.exports = function fn() {
+//if argument is string, reverse is. if it's something else, return null. if its an array, call the function again on each item and return array of results.
+module.exports = function fn(arg) {
+    if (typeof arg !== 'string') {
+        if (Array.isArray(arg)) {
+            let results = [];
+            for (let i = 0; i < arg.length; i++) {
+                results.push(fn(arg[i]));
+            }
+            return results;
+        }
+        return null;
+    }
+    return reverseString(arg);
+
+    function reverseString(str) {
+        return str
+            .split('')
+            .reverse()
+            .join('');
+    }
 };
