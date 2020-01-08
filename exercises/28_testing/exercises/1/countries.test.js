2de7af0ffab537845e1c515543a9b2e4cb696231
David Durlan <david.a.durlan@gmail.com>
2020-01-08 17:31:45 +0100

    first test passing

--- exercises/28_testing/exercises/1/countries.test.js
+++ exercises/28_testing/exercises/1/countries.test.js
@@ -1,1 +1,6 @@
 const countries = require('./countries');
+
+test('When find is passed an empty string, it returns an empty array', () => {
+    const result = countries.find('');
+    expect(result).toEqual([]);
+});
