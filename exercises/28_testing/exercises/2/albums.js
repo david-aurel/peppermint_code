788cf5ad1eb9c5c256df5fa466aba032b1ce6740
David Durlan <david.a.durlan@gmail.com>
2020-01-08 18:22:30 +0100

    start working on 2) spotify, but its not working yet

--- exercises/28_testing/exercises/2/albums.js
+++ exercises/28_testing/exercises/2/albums.js
@@ -4,11 +4,26 @@ exports.getAlbumNames = function(q) {
     return spotify.search(q, 'album').then(function(data) {
         const albumNames = [];
         for (var i = 0; i < data.albums.items.length; i++) {
-            albumNames.push(
-                data.albums.items[i].name
-            );
+            albumNames.push(data.albums.items[i].name);
         }
         albumNames.sort();
         return albumNames;
     });
 };
+
+function getAlbumNames(q) {
+    return spotify.search(q, 'album').then(function(data) {
+        console.log(data.albums.items);
+
+        const albumNames = [];
+        for (var i = 0; i < data.albums.items.length; i++) {
+            albumNames.push(data.albums.items[i].name);
+        }
+        albumNames.sort();
+        console.log(albumNames);
+
+        return albumNames;
+    });
+}
+
+getAlbumNames('meat loaf');
--- exercises/28_testing/exercises/2/albums.test.js
+++ exercises/28_testing/exercises/2/albums.test.js
@@ -1,12 +1,20 @@
-const {getAlbumNames} = require('./albums');
+const { getAlbumNames } = require('./albums');
 const spotify = require('./spotify');
 
-jest.mock('./spotify')
+jest.mock('./spotify');
 
 test('album names are in alphabetical order', () => {
-    return getAlbumNames('meat loaf').then(
-        albumNames => {
-            expect(albumNames).toEqual(albumNames.sort());
+    spotify.search.mockResolvedValue({
+        albums: {
+            items: [
+                { name: 'Bat Out Of Hell' },
+                { name: 'Bat Out Of Hell2' },
+                { name: 'Bat Out Of Hell3' }
+            ]
         }
-    );
+    });
+
+    return getAlbumNames('meat loaf').then(albumNames => {
+        expect(albumNames).toEqual(albumNames.sort());
+    });
 });
