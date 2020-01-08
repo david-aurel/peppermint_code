dc776415f47606f9975c32e2f4dc0e26cf1caea3
David Durlan <david.a.durlan@gmail.com>
2020-01-08 19:36:01 +0100

    make 2. (spotify) work

--- exercises/28_testing/exercises/2/albums.js
+++ exercises/28_testing/exercises/2/albums.js
@@ -10,20 +10,3 @@ exports.getAlbumNames = function(q) {
         return albumNames;
     });
 };
-
-function getAlbumNames(q) {
-    return spotify.search(q, 'album').then(function(data) {
-        console.log(data.albums.items);
-
-        const albumNames = [];
-        for (var i = 0; i < data.albums.items.length; i++) {
-            albumNames.push(data.albums.items[i].name);
-        }
-        albumNames.sort();
-        console.log(albumNames);
-
-        return albumNames;
-    });
-}
-
-getAlbumNames('meat loaf');
--- exercises/28_testing/exercises/2/albums.test.js
+++ exercises/28_testing/exercises/2/albums.test.js
@@ -1,18 +1,19 @@
 const { getAlbumNames } = require('./albums');
 const spotify = require('./spotify');
-
 jest.mock('./spotify');
 
+const mockData = {
+    albums: {
+        items: [
+            { name: 'Bat Out Of Hell' },
+            { name: 'Bat Out Of Hell2' },
+            { name: 'Bat Out Of Hell3' }
+        ]
+    }
+};
+
 test('album names are in alphabetical order', () => {
-    spotify.search.mockResolvedValue({
-        albums: {
-            items: [
-                { name: 'Bat Out Of Hell' },
-                { name: 'Bat Out Of Hell2' },
-                { name: 'Bat Out Of Hell3' }
-            ]
-        }
-    });
+    spotify.search.mockResolvedValue(mockData);
 
     return getAlbumNames('meat loaf').then(albumNames => {
         expect(albumNames).toEqual(albumNames.sort());
